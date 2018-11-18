463d8ea4e0790dd2d872cb944cffd81e1a92ff89
Chaihyun Lee <clee0505@uchicago.edu>
2018-11-17 23:52:55 -0600

    added a partially functional gui

--- human_player.py
+++ human_player.py
@@ -7,6 +7,7 @@ class HumanPlayer(Player):
     def __init__(self, name):
         super(HumanPlayer, self).__init__(name)
         self.shot_record = np.zeros((10,10))
+        self.visualize()
 
     def message(self, s):
         return '[{}]: {}'.format(self.name, s)
@@ -14,7 +15,25 @@ class HumanPlayer(Player):
     def name(self):
         return self.name
 
+    def visualize(self):
+        import matplotlib.pyplot as plt
+        plt.ion()
+        self.fig = plt.figure(figsize=(8,4))
+        self.ax = self.fig.add_subplot(121)
+        self.ax.set_title('Place your ships!')
+        self.ax.set_xticks(np.arange(10))
+        self.ax.set_yticks(np.arange(10))
+        
+        self.ax2 = self.fig.add_subplot(122)
+        self.ax2.set_title('Shoot your enemy ships!')
+        self.ax2.set_xticks(np.arange(10))
+        self.ax2.set_yticks(np.arange(10))
+
+        self.ax.imshow(self.board.board, aspect='auto')
+        self.ax2.imshow(self.shot_record, aspect='auto')
+
     def place_ships(self):
+        # Visualization for placing your ship
         for ship in SHIPS.values():
             while True:
                 try:
@@ -22,6 +41,7 @@ class HumanPlayer(Player):
                     x, y = int(x), int(y)
                     self.board.place_ship(x, y, o, ship)
                     self.board.print_board()
+                    self.ax.imshow(self.board.board)
                     break
                 except KeyboardInterrupt:
                     print('\nQuitting game...')
@@ -39,6 +59,7 @@ class HumanPlayer(Player):
                 x, y = int(x), int(y)
                 shot = self.enemy.shoot(x, y)
                 self.shot_record[x,y] = shot
+                self.ax2.imshow(self.shot_record)
                 break
             except KeyboardInterrupt:
                 print('\nQuitting game...')
